635f29c6ad04fd167e6bb673c2fc7477a410f66c
must-compute <admin@must-compute.com>
2023-10-23 19:04:43 -0400

    remove tui.hpp + add walking skeleton
    walking skeleton components:
    - search: just list all files in working dir
    - navigate
    - preview file content
    - open file in vim
    
    TUI directly uses ncurses

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
-#include "single_include/tui/tui.hpp"
 #include <filesystem>
 #include <vector>
 #include <string>
-
-std::vector<std::string> list_directory_relative_to_home() {
-    std::vector<std::string> result;
-    const auto home_path = std::filesystem::path(std::getenv("HOME"));
-    const auto current_path = std::filesystem::current_path();
-    for (const auto &entry: std::filesystem::directory_iterator(current_path)) {
-        const auto relative_path = std::filesystem::relative(entry.path(), home_path);
-        result.push_back("~/" + relative_path.string());
-    }
-    return result;
-}
-
+#include <ncurses.h>
+#include <optional>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
 
 std::vector<std::string> list_directory() {
     std::vector<std::string> result;
@@ -23,42 +15,103 @@ std::vector<std::string> list_directory() {
     return result;
 }
 
-int main() {
-    tui::Window window;
+std::unordered_map<std::string, std::optional<std::string>> read_directory_files() {
+    std::unordered_map<std::string, std::optional<std::string>> result;
+    const auto current_path = std::filesystem::current_path();
 
-    window.set_title("ff");
-
-    tui::List l;
-    l.title = "results for SEARCH_QUERY in MY/CURRENT/PATH";
-    l.rows = list_directory_relative_to_home();
-    l.set_dimensions(0, 0, 80, 8);
-
-    bool quit = false;
-    tui::Event event;
-
-    while (!quit) {
-        if (window.poll_event(event)) {
-            if (event.type == tui::KEYDOWN) {
-                switch (event.key) {
-                    case 'q':
-                        quit = true;
-                        break;
-                    case 'j':
-                        l.scroll_down(window);
-                        break;
-                    case 'k':
-                        l.scroll_up(window);
-                        break;
-                }
+    for (const auto &entry: std::filesystem::directory_iterator(current_path)) {
+        if (std::filesystem::is_regular_file(entry.status())) {
+            std::ifstream file(entry.path(), std::ios::binary);
+            if (file) {
+                std::stringstream ss;
+                ss << file.rdbuf();
+                result[entry.path().string()] = ss.str();
+            } else {
+                result[entry.path().string()] = std::nullopt;
             }
         }
-        // Add list widget to the window
-        // in the while loop to update
-        // the list for any changes.
-        window.add(l);
-        window.render();
+    }
+    return result;
+}
+
+int main() {
+    initscr();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+
+    auto itemContent = read_directory_files();
+    std::vector<std::string> items;
+
+    items.reserve(itemContent.size());
+    for (const auto &pair: itemContent) {
+        items.push_back(pair.first);
     }
 
-    window.close();
+    int highlight = 0;
+    int choice;
+    int offset = 0;
+    const int MAX_DISPLAY = 4;
+
+    while (1) {
+        int max_y, max_x;
+        getmaxyx(stdscr, max_y, max_x);
+
+        auto position_status = std::to_string(highlight).append("/").append(
+                std::to_string(items.size() - 1)).c_str();
+
+        for (int i = 0; i < MAX_DISPLAY; ++i) {
+            if (i + offset == highlight) {
+                attron(A_REVERSE);
+            }
+            mvprintw(i, 0, items[i + offset].c_str());
+            attroff(A_REVERSE);
+        }
+
+        mvhline(MAX_DISPLAY, 0, 0, max_x - strlen(position_status) - 1);
+        mvprintw(MAX_DISPLAY, max_x - strlen(position_status), position_status);
+
+        auto content = itemContent[items[highlight]] ? *itemContent[items[highlight]] : "NOT A TEXT FILE";
+        mvprintw(MAX_DISPLAY + 1, 0, "Preview: %s", content.substr(0, 1000).c_str());
+
+        choice = getch();
+
+        switch (choice) {
+            case KEY_UP:
+                if (highlight > 0) {
+                    --highlight;
+                    if (highlight < offset) {
+                        --offset;
+                    }
+                }
+                clear();
+                refresh();
+                break;
+            case KEY_DOWN:
+                if (highlight < items.size() - 1) {
+                    ++highlight;
+                    if (highlight - offset >= MAX_DISPLAY) {
+                        ++offset;
+                    }
+                }
+                clear();
+                refresh();
+                break;
+            case 10: // Enter key
+                endwin();
+                std::system(("vim " + items[highlight]).c_str());
+                initscr();
+                noecho();
+                keypad(stdscr, TRUE);
+                curs_set(0);
+                break;
+            default:
+                break;
+        }
+
+        if (choice == 'q') break;
+    }
+
+    endwin();
     return 0;
 }
